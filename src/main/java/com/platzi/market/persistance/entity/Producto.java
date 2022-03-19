c4adc6672c7d3bb3ac749ee623c5914071d32840
NeoTRAN001 <neo_tran@outlook.com>
2022-03-18 20:45:22 -0600

    feat: entity producto

---
+++ src/main/java/com/platzi/market/persistance/entity/Producto.java
@@ -0,0 +1,85 @@
+package com.platzi.market.persistance.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "cantidad_stock")
+    private Integer cantidadStock;
+
+    @Column(name = "precio_venta")
+    private Double precioVenta;
+
+    private Boolean estado;
+
+    @Column(name = "codigo_barras")
+    private String codigoBarras;
+
+    private String nombre;
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
