f06472fc4388fdf994842aa13bb2c2964e06e5fd
NeoTRAN001 <neo_tran@outlook.com>
2022-03-18 18:05:20 -0600

    feat: DDD Structure

--- src/main/java/com/platzi/market/web/controller/HolaMundoController.java
+++ src/main/java/com/platzi/market/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.platzi.market;
+package com.platzi.market.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HolaMundoController {
     @GetMapping("/hola")
     public String saludar() {
-        return "Nunca pares de aprender";
+        return "Nunca pares de aprender XD";
     }
 }
