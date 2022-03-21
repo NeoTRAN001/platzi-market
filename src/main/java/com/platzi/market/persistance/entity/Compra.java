a33d686e88a779758369bc6bcd88ed91c519f312
NeoTRAN001 <neo_tran@outlook.com>
2022-03-21 11:38:54 -0600

    feat: create relation tables

--- src/main/java/com/platzi/market/persistance/entity/Compra.java
+++ src/main/java/com/platzi/market/persistance/entity/Compra.java
@@ -2,6 +2,7 @@ package com.platzi.market.persistance.entity;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "compras")
@@ -27,6 +28,9 @@ public class Compra {
     @ManyToOne
     @JoinColumn(name = "id_cliente", insertable = false, updatable = false)
     private Cliente cliente;
+
+    @OneToMany(mappedBy = "producto")
+    private List<ComprasProducto> productos;
 
     public Integer getIdCompra() {
         return idCompra;
